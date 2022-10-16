a879861b77f23dc5ecc97631d7bbcfc17d2715f9
Maria Gabriela Lopez <47748533+magalopez@users.noreply.github.com>
2022-10-15 22:38:05 -0500

    change promise to async await, add readme

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const baseUrl = "https://platzi-avo.vercel.app"
 const url = "/api/avo";
 
 //Intl
-
 function formatPrice (price) {
   const newPrice = new window.Intl.NumberFormat('en-EN', {
     style: 'currency',
@@ -23,41 +22,47 @@ function formatPrice (price) {
 //web api
 //Connect to Server
 //promise -> async/await
-window
-.fetch(`${baseUrl}${url}`)
-//Convert response to json
-.then(response => response.json())
-.then(responseJson => {
-    const cards = [];
-    responseJson.data.forEach(item => {
-      //image
-      const image = document.createElement('img')
-      document.body.appendChild(image)
-      image.setAttribute('src', `${baseUrl}${item.image}`);
-      image.className = "w-24 rounded-full"
-      //title
-      const title = document.createElement('h2')
-      document.body.appendChild(title)
-      title.innerText = item.name;
-      // title.style = "font-size: 2rem"
-      // title.style.fontSize = "3rem"
-      title.className = "text-base"
+const getData = async (baseUrl, url) => {
+  const response = await fetch(`${baseUrl}${url}`);
+  const responseJson = await response.json();
+  return responseJson.data;
+}
 
-      //price
-      const price = document.createElement('div')
-      price.innerText = formatPrice(item.price);
-      price.className = "text-gray-600";
+const data = await getData(baseUrl, url);
 
-      const wrapperRight = document.createElement('div');
-      wrapperRight.append(title, price)
-      wrapperRight.className = "flex flex-col items-start justify-center p-4";
+const cards = [];
 
-      const container = document.createElement('div');
-      container.append(image, wrapperRight);
-      container.className = "flex flex-row bg-gray-200 rounded-xl p-4 hover:bg-gray-500 hover:text-white cursor-pointer"
-      cards.push(container);
-    });
-    appNode.className = "container mx-auto grid gap-4 grid-cols-3 grid-rows-3 mt-8"
-    appNode.append(...cards)
-  });
+data.forEach(item => {
+  //image
+  const image = document.createElement('img');
+  document.body.appendChild(image);
+  image.setAttribute('src', `${baseUrl}${item.image}`);
+  image.className = "w-24 rounded-full";
 
+  //title
+  const title = document.createElement('h2');
+  document.body.appendChild(title);
+  title.innerText = item.name;
+  // title.style = "font-size: 2rem"
+  // title.style.fontSize = "3rem"
+  title.className = "text-base";
+
+  //price
+  const price = document.createElement('div')
+  price.innerText = formatPrice(item.price);
+  // price.className = "text-gray-600";
+
+  const wrapperRight = document.createElement('div');
+  wrapperRight.append(title, price);
+  wrapperRight.className = "flex flex-col items-start justify-center p-4";
+
+  const container = document.createElement('div');
+  container.append(image, wrapperRight);
+  container.className = "flex flex-row bg-gray-200 rounded-xl p-4 hover:bg-gray-500 hover:text-white cursor-pointer"
+  cards.push(container);
+})
+
+appNode.className = "container mx-auto grid gap-4 grid-cols-3 grid-rows-3 mt-8";
+appNode.append(...cards);
+
+console.log('Happy ending :)');
